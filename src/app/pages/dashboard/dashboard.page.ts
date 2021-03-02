4b8f248835ccb8f9c6f72211493dca42995392c6
Dev-Yol <leonilojr.torres@student.passerellesnumeriques.org>
2021-03-02 19:36:52 +0800

    Initial Push

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -40,12 +40,12 @@ export class DashboardPage implements OnInit {
             label: "# of Votes",
             data: arrayForData,
             backgroundColor: [
-              // "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
-              // "rgba(255, 206, 86, 0.2)",
-              // "rgba(75, 192, 192, 0.2)",
-              // "rgba(153, 102, 255, 0.2)",
-              // "rgba(255, 159, 64, 0.2)"
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
             ],
             borderColor: [
               "rgba(255,99,132,1)",
