75e6e6a95f873e44c5cdbb34bdf2510cf614b32e
Dev-Yol <leonilojr.torres@student.passerellesnumeriques.org>
2021-03-02 14:46:48 +0800

    Initial Push

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { DataRequestsService } from '../../request-to-BE/data-requests.service'
-import { RequestsService } from '../../logInAndSignupService/requests.service'
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +7,62 @@ import { RequestsService } from '../../logInAndSignupService/requests.service'
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  @ViewChild("barCanvas",  { static: true }) barCanvas: ElementRef;
+  @ViewChild("lineCanvas", {static: true}) lineCanvas: ElementRef;
+
+  public arrayOfCellgroup = [20, 100, 20, 20]
+  public arrayOfSundayCeleb = [20, 60, 100, 20]
+
+  private barChart: Chart;
 
   constructor() { }
 
   ngOnInit() {
+    this.graphCreated(this.barCanvas, this.arrayOfCellgroup);
+    this.graphCreated(this.lineCanvas, this.arrayOfSundayCeleb);
+  }
+
+  graphCreated(params, arrayForData) {
+    this.barChart = new Chart(params.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: arrayForData,
+            backgroundColor: [
+              // "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              // "rgba(255, 206, 86, 0.2)",
+              // "rgba(75, 192, 192, 0.2)",
+              // "rgba(153, 102, 255, 0.2)",
+              // "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 0
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
   }
 
 }
